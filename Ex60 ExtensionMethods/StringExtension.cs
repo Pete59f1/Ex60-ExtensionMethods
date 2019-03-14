f0f381ce115ed145c9d18c4fe326669080921843
Peter Starup <pete59f1@edu.eal.dk>
2019-03-14 10:11:43 +0100

    Add project files.

---
+++ Ex60 ExtensionMethods/StringExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex60_ExtensionMethods
+{
+    public static class StringExtension
+    {
+        public static string Shift(this string str, int shift)
+        {
+            string temp = str.Substring(str.Length - shift);
+            if (shift > 0)
+            {
+                str = str.Remove(str.Length - shift);
+            }
+            
+            return str.Insert(0, temp);
+        }
+    }
+}
---
+++ Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d04c69ee-b8af-474a-be8e-f954d7f9b7d8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
